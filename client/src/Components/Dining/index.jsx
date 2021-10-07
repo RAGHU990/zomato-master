0ecc46b176dd90d0c5e54caeede066c65ed46f8f
Raghuram <raghu17122001@gmail.com>
2021-10-07 23:55:36 +0530

    add dining carousal

---
+++ client/src/Components/Dining/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import PictureCarousalCard from "../PictureCarousal";
+
+// Components
+import DiningCarousal from "./DiningCarousal";
+
+const Dining = () => {
+    return (
+        <div>
+          <DiningCarousal/> 
+          <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">Dine-Out Restaurant in Kalavasal</h1> 
+        
+        </div>
+    );
+};
+
+export default Dining;
