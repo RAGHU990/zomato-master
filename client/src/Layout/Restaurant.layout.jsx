6078aa94d364cbd0732aa23037c7ff769092ca38
Raghuram <raghu17122001@gmail.com>
2021-10-14 19:15:27 +0530

    add cart

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -9,6 +9,7 @@ import ImageGrid from "../Components/restaurant/ImageGrid";
 import RestaurantInfo from "../Components/restaurant/RestaurantInfo";
 import InfoButtons from "../Components/restaurant/InfoButtons";
 import TabContainer from "../Components/restaurant/Tabs";
+import CartContainer from "../Components/Cart/CartContainer";
 const RestaurantLayout = (props) => {
   return (
     <>
@@ -50,6 +51,7 @@ const RestaurantLayout = (props) => {
         </div>
         { props.children }
       </div>
+      <CartContainer />
     </>
   );
 };
